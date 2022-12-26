76012efa94427778af7fbea1a47f46b07711734f
Alexab <palexab@gmail.com>
2022-12-26 17:48:53 +0300

    Add command-line interface

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <boost/program_options.hpp>
 
 #include "XNetSDK/include/XNetSDK.h"
 #include "PtzCameraControl.h"
@@ -33,8 +34,34 @@ int CALLBACK Main_MessageCallBack(XSDK_HANDLE hDevice, int nMsgId, int nParam1, 
   return 1;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+  boost::program_options::options_description desc("Allowed options");
+  desc.add_options()(
+    "address,a",
+    boost::program_options::value< std::string >()->default_value(""),
+    "Camera address"
+  )(
+    "port,p",
+    boost::program_options::value< int >()->default_value(80),
+    "Camera port"
+  )(
+    "username,U",
+    boost::program_options::value< std::string >()->default_value("root"),
+    "User name"
+  )(
+    "password,P",
+    boost::program_options::value< std::string >()->default_value("root"),
+    "User password"
+  );
 
+  boost::program_options::variables_map settings;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), settings);
+  boost::program_options::notify(settings);
+
+  if(settings["address"].as<std::string>().empty()) {
+    std::cerr << desc << std::endl;
+    return 1;
+  }
   SXSDK_Version pVersionInfo;
 
   XSDK_GetXNetSDKVersion(&pVersionInfo);
@@ -48,7 +75,8 @@ int main() {
   int init_result = XSDK_Init(&initParam);
   std::cout << "SDK init status: " << init_result << std::endl;
 
-  PtzCameraControl cam("http://10.10.10.10", 80, "root", "root");
+//  PtzCameraControl cam("http://10.10.10.10", 80, "root", "root");
+  PtzCameraControl cam(settings["address"].as<std::string>(), settings["port"].as<int>(), settings["username"].as<std::string>(), settings["password"].as<std::string>());
 
   cam.connect();
   XSDK_HANDLE device_handle = cam.get_device_handle();
